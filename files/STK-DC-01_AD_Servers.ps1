bf7a1acfd5c811c38a75acc6dbc2a9b186a3f19f
Q <135470484+phybroptyx@users.noreply.github.com>
2024-07-20 22:43:02 -0600

    Removed unused files
    Removed the following unused files:
    
    STK-DC-01_AD_OU_Config.ps1
    STK-DC-01_AD_Servers.ps1
    STK-DC-01_AD_Sites_Config.ps1
    STK-DC-01_DNS_Config.ps1
    STK-DC-01_AD_Workstations.ps1

--- files/STK-DC-01_AD_Servers.ps1
+++
@@ -1,5 +0,0 @@
-New-ADComputer -Name "stk-ex-01" -SamAccountName "stk-ex-01" -Path "OU=Servers,OU=STARK,DC=stark-industries,DC=midgard,DC=mrvl"
-New-ADComputer -Name "stk-as-01" -SamAccountName "stk-as-01" -Path "OU=Servers,OU=STARK,DC=stark-industries,DC=midgard,DC=mrvl"
-New-ADComputer -Name "stk-lhm-01" -SamAccountName "stk-lhm-01" -Path "OU=Servers,OU=STARK,DC=stark-industries,DC=midgard,DC=mrvl"
-New-ADComputer -Name "stk-fhm-01" -SamAccountName "stk-fhm-01" -Path "OU=Servers,OU=STARK,DC=stark-industries,DC=midgard,DC=mrvl"
-New-ADComputer -Name "stk-hst-01" -SamAccountName "stk-hst-01" -Path "OU=Servers,OU=STARK,DC=stark-industries,DC=midgard,DC=mrvl"
--- files/STK-DC-01_AD_Sites_Config.ps1
+++
@@ -1,2 +0,0 @@
-Rename-ADObject -Identity "CN=Default-First-Site-Name,CN=Sites,CN=Configuration,DC=stark-industries,DC=midgard,DC=mrvl" -NewName "NYC"
-New-ADReplicationSubnet -Name "37.186.49.0/24" -Site "NYC"
